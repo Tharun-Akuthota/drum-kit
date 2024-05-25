61323ac3446c8289151367bb7cc3e97325c3404c
Tharun Akuthota <124380030+Tharun-Akuthota@users.noreply.github.com>
2024-05-25 12:36:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,31 +15,31 @@ document.addEventListener("keypress", function(event) {
 
 function makeSound(key) {
   switch (key) {
-    case "w":
+    case "t":
       var tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
       break;
-    case "a":
+    case "h":
       var tom2 = new Audio("sounds/tom-2.mp3");
       tom2.play();
       break;
-    case "s":
+    case "a":
       var tom3 = new Audio('sounds/tom-3.mp3');
       tom3.play();
       break;
-    case "d":
+    case "r":
       var tom4 = new Audio('sounds/tom-4.mp3');
       tom4.play();
       break;
-    case "j":
+    case "u":
       var snare = new Audio('sounds/snare.mp3');
       snare.play();
       break;
-    case "k":
+    case "n":
       var crash = new Audio('sounds/crash.mp3');
       crash.play();
       break;
-    case "l":
+    case "#":
       var kick = new Audio('sounds/kick-bass.mp3');
       kick.play();
       break;
